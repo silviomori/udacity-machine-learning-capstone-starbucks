5981bca2148e10b7df0abb11b1f5d6f6b7a66d04
Silvio Mori Neto <silvio@technomori.com>
2020-02-07 15:04:57 -0300

    feat: Build and train the recurrent classifier

--- models.py
+++ models.py
@@ -30,3 +30,55 @@ class LinearNN(nn.Module):
 
         return x
 
+
+class RecurrentNN(nn.Module):
+    def __init__(self, input_size, hidden_size=128, hidden_layers=2,
+                 classifier_layers=[128], output_size=1, dropout=0.2):
+        super(RecurrentNN, self).__init__()
+
+        self.input_size = input_size
+        self.hidden_size = hidden_size
+        self.output_size = output_size
+
+        ## Build the Recurrent Network
+        self.rnn = nn.RNN(input_size=input_size,
+                          hidden_size=hidden_size,
+                          num_layers=hidden_layers,
+                          nonlinearity='tanh',
+                          batch_first=True,
+                          dropout=dropout)
+
+        ## Build the classifier
+        nodes = []
+        nodes.append(hidden_size)
+        nodes.extend(classifier_layers)
+        nodes.append(output_size)
+        self.classifier = nn.ModuleList()
+        for nodes_in, nodes_out in zip(nodes[:-1], nodes[1:]):
+            layer = nn.Linear(nodes_in, nodes_out)
+            self.classifier.append(layer)
+
+        self.activation = nn.Tanh()
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, x, h):
+        batch_size = x.shape[0]
+        seq_length = x.shape[1]
+
+        ## Feed the RNN
+        x, h = self.rnn(x, h)
+
+        ## Feed the Classifier
+        x = x.contiguous().view(-1, self.hidden_size)
+        x = self.dropout(x)
+        for layer in self.classifier[:-1]:
+            x = layer(x)
+            x = self.activation(x)
+            x = self.dropout(x)
+        x = self.classifier[-1](x)
+
+        ## Stack the results
+        x = x.view(batch_size, seq_length, self.output_size)
+
+        return x, h
+
